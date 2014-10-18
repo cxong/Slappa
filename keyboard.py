471d7ba458428d176c020287f243fbe42bd56aa8
Cong <congusbongus@gmail.com>
2014-10-18 13:50:48 +1100

    Custom resolution and controls for GCW Zero
    Clean up cat sprite

--- keyboard.py
+++ keyboard.py
@@ -1,9 +1,20 @@
 import pygame
+from config import *
 
 
 class Keyboard:
     def __init__(self):
         self.keys = None
+        self.left = pygame.K_LEFT
+        self.right = pygame.K_RIGHT
+        self.jump = pygame.K_UP
+        self.hit_left = pygame.K_a
+        self.hit_right = pygame.K_d
+        self.hit_up = pygame.K_w
+        if GCW_ZERO:
+            self.hit_left = pygame.K_LSHIFT
+            self.hit_right = pygame.K_LCTRL
+            self.hit_up = pygame.K_SPACE
 
     def update(self):
         self.keys = pygame.key.get_pressed()
@@ -14,22 +25,22 @@ class Keyboard:
     def dir(self):
         if self.keys is None:
             return 0
-        if self.keys[pygame.K_LEFT]:
+        if self.keys[self.left]:
             return -1
-        elif self.keys[pygame.K_RIGHT]:
+        elif self.keys[self.right]:
             return 1
         return 0
 
     def is_jump(self):
-        return self.keys is not None and self.keys[pygame.K_UP]
+        return self.keys is not None and self.keys[self.jump]
 
     def hit(self):
         if self.keys is None:
             return ""
-        if self.keys[pygame.K_a]:
+        if self.keys[self.hit_left]:
             return "left"
-        elif self.keys[pygame.K_d]:
+        elif self.keys[self.hit_right]:
             return "right"
-        elif self.keys[pygame.K_w]:
+        elif self.keys[self.hit_up]:
             return "up"
         return ""
