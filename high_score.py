991ba907763d1822d22d19a4b50ef8cf0a684932
Cong <congusbongus@gmail.com>
2014-10-24 22:15:07 +1100

    Portable get app data for high scores

--- high_score.py
+++ high_score.py
@@ -3,12 +3,37 @@ import errno
 import locale
 import os
 import pickle
+import sys
+
+
+"""
+Douglas Mayle
+http://stackoverflow.com/users/8458/douglas-mayle
+http://stackoverflow.com/a/1088459/2038264
+http://creativecommons.org/licenses/by-sa/3.0/
+"""
+
+
+def get_app_data(app_name):
+    if sys.platform == 'darwin':
+        from AppKit import NSSearchPathForDirectoriesInDomains
+        # http://developer.apple.com/DOCUMENTATION/Cocoa/Reference/Foundation/Miscellaneous/Foundation_Functions/Reference/reference.html#//apple_ref/c/func/NSSearchPathForDirectoriesInDomains
+        # NSApplicationSupportDirectory = 14
+        # NSUserDomainMask = 1
+        # True for expanding the tilde into a fully qualified path
+        app_data = os.path.join(
+            NSSearchPathForDirectoriesInDomains(14, 1, True)[0], app_name)
+    elif sys.platform == 'win32':
+        app_data = os.path.join(os.environ['APPDATA'], app_name)
+    else:
+        app_data = os.path.expanduser(os.path.join("~", "." + app_name))
+    return app_data
 
 
 class HighScore(object):
     def __init__(self):
         locale.setlocale(locale.LC_TIME, '')
-        self.dir = os.path.join(os.path.expanduser('~'), '.slappa')
+        self.dir = get_app_data('slappa')
         self.filename = os.path.join(self.dir, 'scores.dat')
         # Load existing high scores
         try:
